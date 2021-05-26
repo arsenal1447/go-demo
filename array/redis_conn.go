94bd8a2ab8ec86af08bfe317277e583ef0e1d4d2
kentzhou <6026852+arsenal1447@users.noreply.github.com>
2021-05-26 14:57:54 +0800

    Create redis_conn.go

---
+++ array/redis_conn.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/go-redis/redis"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+// 定义一个全局对象rdb
+var rdb *redis.Client
+
+// 初始化连接
+func initClient() (err error) {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "zxx123456", // no password set
+		DB:       0,           // use default DB
+		PoolSize: 100,         // 连接池大小
+	})
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	_, err = rdb.Ping(ctx).Result()
+	if err != nil {
+		return err
+	}
+	return nil
+
+}
+
+func V8Example() {
+	ctx := context.Background()
+	if err := initClient(); err != nil {
+		return
+	}
+
+	err := rdb.Set(ctx, "key", "value", 0).Err()
+	if err != nil {
+		panic(err)
+	}
+	val, err := rdb.Get(ctx, "key").Result()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("key", val)
+
+	val2, err := rdb.Get(ctx, "key2").Result()
+	if err == redis.Nil {
+		fmt.Println("key2 does not exist")
+	} else if err != nil {
+		panic(err)
+	} else {
+		fmt.Println("key2", val2)
+	}
+}
+
+func main() {
+	err := initClient()
+	if err != nil {
+		fmt.Printf("init db failed,err:%v\n", err)
+		return
+	}
+	V8Example()
+}
